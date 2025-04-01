5fc8b893b521ff4b51fbe66fc63069e6a323468b
donald <donaldford05091997@gmail.com>
2025-04-01 01:47:11 -0400

    added the ability to run the python exploit as a module

--- setup/executable.sh
+++ setup/executable.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 cd $HOME
 echo "Making supersploit executable"
-printf '#include <stdio.h>\nint main(){\nsystem("bash $HOME/.SuperSploit/start.sh");}' > supersploit.c
+printf '#include <stdio.h>\n#include <stdlib.h>\nint main(){\nsystem("bash $HOME/.SuperSploit/start.sh");}' > supersploit.c
 
 echo "Compiling executable"
 gcc supersploit.c -o supersploit
 
 echo "Moving  supersploit to /bin folder"
-sudo mv supersploit /bin/supersploit && rm supersploit.c
+sudo cp supersploit /bin/supersploit
 
 echo "Making desktop file and app drawer icon"
-printf "[Desktop Entry]\nName=supersploit\nComment=Exploit Management framework\nExec=/bin/supersploit\nIcon=/usr/share/icons/locolor/16x16/apps/logo1.png\nTerminal=true\nType=Application\nCategories=Exploitation Tools" > supersploit.desktop
+printf "[Desktop Entry]\nName=supersploit\nComment=Exploit Management framework\nExec=/bin/supersploit\nIcon=/usr/share/icons/locolor/16x16/apps/logo1.png\nTerminal=true\nType=Application\nCategories=GTK;System;Core;" > supersploit.desktop
 sudo cp supersploit.desktop /usr/share/applications/
 
 echo "Moving icon"
@@ -19,8 +19,5 @@ sudo cp $HOME/.SuperSploit/.data/.assets/logo1.png /usr/share/icons/locolor/16x1
 echo "Changing permissions for executable file"
 sudo chmod +x /bin/supersploit
 
-echo "Cleaning up"
-# sudo rm supersploit.desktop supersploit.c
 cd $HOME/.SuperSploit
 
-python3 setup.py
